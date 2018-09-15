60f4433036593856907ebee9a3be86aa5699d235
Mischa Jörg <info@ninetyfour.ch>
2018-09-15 11:47:52 +0200

    add score calculation

--- foodie_app/api.py
+++ foodie_app/api.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+import requests
 from foodie_app.models import db
 from foodie_app.models import Ingredient
 from foodie_app.models import Badge
@@ -43,3 +44,24 @@ def getBadgeWith(id):
 @api.route('/', methods=['GET'])
 def test():
     return 'hello world'
+
+@api.route('/recipes', methods=['POST'])
+def get_recipes():
+    db_ingredients = Ingredient.query.all()
+    ingredients = request.json
+    ingredients = ingredients["ingredients"]
+    recipes = list()
+    for ingredient in ingredients:
+        res = requests.get('https://api.edamam.com/search?q={}&app_id=4624bf98&app_key=0b77cc1602cbb9dd8db2f1618f00006c'.format(ingredient))
+        res = res.json().get("hits", None)
+        for recipe in res:
+            score = 0
+            for res_ing in recipe["recipe"]["ingredientLines"]:
+                for ing in ingredients:
+                    if ing in res_ing:
+                        for db_ing in db_ingredients:
+                            if db_ing.name == ing:
+                                score += db_ing.gwp
+            recipes.append({"label": recipe["recipe"]["label"], "uri": recipe["recipe"]["shareAs"], "score": score})
+    recipes = list({recipe["uri"]: recipe for recipe in recipes}.values())
+    return jsonify({"recipes": [{"label": recipe["label"], "uri": recipe["uri"], "score": recipe["score"]} for recipe in recipes]})
