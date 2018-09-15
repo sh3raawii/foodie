b0edce55c24f2203690e68fa25fdc26ac6fbc608
Mischa Jörg <info@ninetyfour.ch>
2018-09-15 03:48:57 +0200

    add api routes

--- foodie_app/api.py
+++ foodie_app/api.py
@@ -1,7 +1,22 @@
 from flask import Blueprint, request, jsonify
-
+from foodie_app.models import Ingredient
+from foodie_app.models import db
 api = Blueprint('api', __name__)
 
+@api.route('/ingredients', methods=["POST"])
+def postNewIngredient():
+    ingredient = Ingredient(gwp=11.34)
+    db.session.add(ingredient)
+    db.session.commit()
+    return jsonify({"id": ingredient.id, "gwp": ingredient.gwp})
+
+@api.route('/ingredients/<id>', methods=["GET"])
+def getIngredientWith(id):
+    ingredient = Ingredient.query.filter(Ingredient.id == id).first()
+    return jsonify({"id": ingredient.id, "gwp": ingredient.gwp})
+
+# @api.route('/badges/<id>')
+# @api.route('/collections/<id>')
 
 @api.route('/', methods=['GET'])
 def test():
